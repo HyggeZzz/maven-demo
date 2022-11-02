c5a1c8d8711374a589fa4db2975c4395f3587626
HyggeZzz <103818181+HyggeZzz@users.noreply.github.com>
2022-11-02 10:21:11 +0800

    github2gitee commit

--- src/main/java/Github2Gitee.java
+++ src/main/java/Github2Gitee.java
@@ -2,5 +2,6 @@ public class Github2Gitee {
     public static void main(String[] args) {
         System.out.println("hello github");
         System.out.println("hello github1");
+        System.out.println("hello github2");
     }
 }
